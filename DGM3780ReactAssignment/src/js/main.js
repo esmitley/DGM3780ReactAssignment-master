3866d1f9c6b040a9b71b53412e468f85eec11427
esmitley <10570176@uvlink.uvu.edu>
2016-05-05 19:08:14 -0600

    commit1

--- DGM3780ReactAssignment/src/js/main.js
+++ DGM3780ReactAssignment/src/js/main.js
@@ -11,3 +11,83 @@ var ReactDOM = require('react-dom');
 ///      Edit below. All JS you need is included above.     ///
 ///////////////////////////////////////////////////////////////
 
+var HeadingCategoryRow = React.createClass({
+  render: function() {
+    return (<tr><th colSpan="2">{this.props.item}</th></tr>);
+  }
+});
+
+var ItemsRow = React.createClass({
+  render: function() {
+    var name = this.props.item
+        this.props.item.name :
+     return (
+      <tr>
+        <td>{name}</td>
+      </tr>
+    );
+  }
+});
+var ItemsTable = React.createClass({
+  render: function() {
+    var rows = [];
+    var lastCategory = null;
+    this.props.items.forEach(function(item) {
+         if (item.category !== lastCategory) {
+     rows.push(<ItemRow item={item} key={item.name} />);
+      lastCategory = item.category;
+    });
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+        
+           </table>
+    );
+  }
+});
+
+var InputBar = React.createClass({
+  render: function() {
+    return (
+      <form>
+        <input type="text" placeholder="Item to do..." />
+        <p>
+          <input type="checkbox" />
+          {' '}
+        
+           </p>
+      </form>
+    );
+  }
+});
+        
+FilterableListTable = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <SearchBar />
+        <ToDoTable items={this.props.items} />
+      </div>
+    );
+  }
+});
+
+
+var ITEMS = [
+  {category: 'Urgent', name: '1'},
+  {category: 'Regular', name: '2'},
+  {category: 'Long', name: '3'},   
+    
+
+];
+ 
+ReactDOM.render(
+  <FilterableListTable items={Items} />,
+  document.getElementById('content')
+);    
+   
